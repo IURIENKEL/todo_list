ce3ad02e9ff66e8cacf8c14c3d2365fdf3b201a5
Iuri <iuri.enkel.klinfi@gmail.com>
2025-05-27 19:51:24 -0400

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('tasks', TaskController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', fn() => redirect()->route('tasks.index'));
+Route::middleware('auth')->group(function () {
+    
+    Route::resource('tasks', TaskController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
